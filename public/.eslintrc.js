271d3250f324141f6a9db506f3dfe7ba99e0ab9e
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-07-03 14:16:19 -0400

    First commit

---
+++ public/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "parserOptions": {
+    "surceTtype": "scriipt",
+  },
+  "env": {
+    "browser": true,
+    "jquery": true,
+    "node": false
+  },
+  "rules":{
+    "no-var": "off",
+    "prefer-const": "off"
+  }
+}
